b083a579f0224322ca0c2f7fc5fb791aad475af8
Jiayue Chen <chenjy527@gmail.com>
2019-08-24 20:43:25 -0400

    load content by ReactDOM.render

--- FirstStepsinReact/2 - Add React to a Project/scoreboard/app.js
+++ FirstStepsinReact/2 - Add React to a Project/scoreboard/app.js
@@ -6,4 +6,20 @@ const title = React.createElement(
     'My first react element!'
 );
 
-console.log(title);
+const desc = React.createElement(
+    'p',
+    null,
+    'I just learned how to create a React node and render it into the DOM.'
+);
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
